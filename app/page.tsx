21ac1a9dea60ee8aa52c6aced89d54ada6eb9ef3
Eric Han <gyhan21@icloud.com>
2026-08-03 17:31:46 +0900

    메인 페이지 노출 순서 변경

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Page() {
   return (
     <main className="bg-background text-foreground">
       <Scene1 />
-      <Scene2 />
       <WinterBanner />
+      <Scene2 />
       <Scene3 />
       <Scene4 />
     </main>
